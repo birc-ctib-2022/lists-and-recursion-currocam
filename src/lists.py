ad9b0d8c8082f1527b30ed3f91c1ff3dd0f532bf
currocam <campuzanocurro@gmail.com>
2022-10-14 08:52:00 +0200

    refactor: drop into match

--- src/lists.py
+++ src/lists.py
@@ -96,8 +96,10 @@ def drop(x: List[T], k: int) -> List[T]:
     >>> drop(x, 4)
     >>> drop(x, 5)
     """
-    if x is None: return x
-    return x if k <= 0 else drop(x.tail, k-1)
+    match (k, x):
+        case (0, x): return x
+        case (_, None): return None
+        case (k, L(_, tail)): return drop(tail, k-1)
 
 def keep(x: List[T], k: int) -> List[T]:
     """
